90e97edd83933aeda60251e5aad64faee85f0b31
ttuzz <ozcan_oz_10@hotmail.com>
2015-04-10 18:23:07 +0300

    add

--- uart.c
+++ uart.c
@@ -6,10 +6,10 @@ P1OUT = 0;                                // All P1.x reset
 void HardwareUart(void){
 	P1SEL = BIT1 + BIT2;
 	P1SEL2 = BIT1 + BIT2;
+	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
 }
 void 1Mhz_9600Baud(void){
 	HardwareUart();
-	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
 	UCA0BR0 = 104;                            // 1MHz 9600
 	UCA0BR1 = 0;                              // 1MHz 9600
 	UCA0MCTL = UCBRS0;
@@ -17,9 +17,15 @@ void 1Mhz_9600Baud(void){
 }
 void 1Mhz_11500Baud(void){
 	HardwareUart();
-	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
 	UCA0BR0 = 8;                              // 1MHz 115200
 	UCA0BR1 = 0;                              // 1MHz 115200
 	UCA0MCTL = UCBRS2 + UCBRS0;               // Modulation UCBRSx = 5
+	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+}
+void 16Mhz_9600Baud(void){
+	HardwareUart();
+	UCA0BR0 = 130;                            // 16MHz 9600
+	UCA0BR1 = 6;                              // 16MHz 9600
+	UCA0MCTL = UCBRS2 + UCBRS1;               // modulation is 110b or 6
 	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
 }
